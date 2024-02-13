7f5343bbd258ec97ff34949927eec23ce6fa6383
johnrichardsson <linguisticsrichardsson@gmail.com>
2024-02-13 00:26:19 -0500

    build 0.3.12.3

--- app/index.js
+++ app/index.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { View, ScrollView, SafeAreaView, Text } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
-import { COLORS, IMAGES, FONTS } from '../constants';
-import { Component1 } from '../components/Cards/IntroCards'
+import { COLORS } from '../constants';
+import { Component1 } from '../components';
 
 const Home = () => {
     const router = useRouter();
